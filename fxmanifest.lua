839f2f1d6df0429fef02685aa93a5d8173a354f6
Timo Steinbrech <46078258+timo-steinbrech@users.noreply.github.com>
2023-01-16 17:42:57 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "phone_contract"
+description "This little script is about your phone contract."
+author "Nephren"
+version "1.0.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
